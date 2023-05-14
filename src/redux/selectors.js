21d3f3ddc91ca301400a7a6be3d7f190678b176c
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-05-14 13:30:48 +0200

    finito!

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,11 +1,16 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectConatcts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter.filter;
-export const selectVisibleContacts = state => {
-  const contacts = selectConatcts(state);
-  const normalizedFilter = selectFilter(state).toLowerCase();
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter)
-  );
-};
+
+export const selectVisibleContacts = createSelector(
+  [selectConatcts, selectFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
